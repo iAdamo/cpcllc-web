c2468a8d87de8b60a607a78e9e63282fe1d9e6a6
Adam_Babatunde_Sanusi <tundey520@gmail.com>
2025-04-24 12:42:52 +0100

    carousel images

--- public/assets/homepage/index.ts
+++ public/assets/homepage/index.ts
@@ -1,4 +1,3 @@
-export { default as HomeHeada } from './home-header.jpg';
 export { default as HomeCompany } from './home-company.jpg';
 export { default as HomeClient } from './home-client.jpg';
 export { default as Categ0 } from './categ0.jpeg';
