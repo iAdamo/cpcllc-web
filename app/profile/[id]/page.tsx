70995b7a729a6bc2a41172f4ca2067671f372aa6
Adam Sanusi Babatunde <tundey520@gmail.com>
2026-05-28 20:16:09 +0100

    build error fix

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,8 +1,51 @@
+import type { Metadata } from "next";
+import { getUserProfile } from "@/axios/user";
+import { MediaItem } from "@/types";
+
 import ProfilePage from "@/screens/profile";
 
-export default ProfilePage;
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const user = await getUserProfile(params.id);
+  const provider = user?.activeRole === "Provider" ? user.activeRoleId : null;
 
-export const metadata = {
-  title: "Profile | CompaniesCenterLLC",
-  description: "View and Edit Your Profile",
-};
+  const title = provider?.providerName;
+
+  const description =
+    user?.activeRole === "Provider"
+      ? `${provider?.providerDescription?.slice(0, 100)}${
+          (provider?.providerDescription?.length ?? 0) > 100 ? "..." : ""
+        }`
+      : "";
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        ...(provider?.providerLogo
+          ? [(provider.providerLogo as MediaItem).thumbnail]
+          : []),
+        ...((provider?.providerImages ?? []) as unknown as MediaItem[])
+          .slice()
+          .map((img) => img.thumbnail)
+          .filter((v): v is string => Boolean(v)),
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: provider?.providerLogo
+        ? [(provider.providerLogo as MediaItem).thumbnail]
+        : [],
+    },
+  };
+}
+
+export default ProfilePage;
